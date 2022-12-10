d1043d4a131b8a4aa801db733d66fc09e4b6dfe0
mtinderholt <mtinderholt@microsoft.com>
2022-12-10 18:14:29 -0500

    move the helm

--- k8s/main.tf
+++ k8s/main.tf
@@ -4,26 +4,3 @@ resource "kubernetes_namespace" "main" {
     name = "markti"
   }
 }
-
-resource "helm_release" "azure_csi" {
-  name       = "csi"
-
-  repository = "https://azure.github.io/secrets-store-csi-driver-provider-azure/charts"
-  chart      = "csi-secrets-store-provider-azure"
-
-  namespace = "kube-system"
-/*
-  set {
-    name = "app.kubernetes.io/managed-by"
-    value = "Helm"
-  }
-  set {
-    name = "meta.helm.sh/release-name"
-    value = "azure-csi"
-  }
-  set {
-    name = "meta.helm.sh/release-namespace"
-    value = "kube-system"
-  }*/
-
-}
